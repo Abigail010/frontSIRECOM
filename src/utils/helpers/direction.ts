1befd4c2a2dd23698cca8778fe533fcc10894864
ArielFernandez111 <arielfernandez.rma7@gmail.com>
2025-01-23 15:53:13 -0400

    cambio en direction para la exportacion de la constante

--- src/utils/helpers/direction.ts
+++ src/utils/helpers/direction.ts
@@ -1,3 +1,2 @@
 const dev = false; // Cambia a false en producción
-const URL = dev ? 'http://localhost:3004' : 'https://api.mingobierno.gob.bo/sirecomapi/';
-export { URL }
+export const URL = dev ? 'http://localhost:3004' : 'https://api.mingobierno.gob.bo/sirecomapi/';
