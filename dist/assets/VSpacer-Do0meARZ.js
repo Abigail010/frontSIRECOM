d6c289a72415b5746ccbbe1a23a5b5a166a34a1d
arielfernandez111 <arielfernandez.rma7@gmail.com>
2025-04-21 13:37:10 -0400

    actualizacion para produccion

---
+++ dist/assets/VSpacer-Do0meARZ.js
@@ -0,0 +1,1 @@
+import"./VRow-DQLErQmL.js";import{c as r}from"./VAvatar-zQgJEEQ6.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- dist/assets/transition-CbZCXkoV.js
+++
@@ -1,1 +0,0 @@
-import{p,b9 as c,v as m,aC as l}from"./index-Dy25lhf-.js";const d=p({transition:{type:[Boolean,String,Object],default:"fade-transition",validator:o=>o!==!0}},"transition"),f=(o,n)=>{let{slots:a}=n;const{transition:t,disabled:s,...r}=o,{component:e=l,...i}=typeof t=="object"?t:{};return c(e,m(typeof t=="string"?{name:s?"":t}:i,r,{disabled:s}),a)};export{f as M,d as m};
